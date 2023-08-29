444bbeff1968ae03890cedac7071067cf45e9ead
Majlo <szmilosz01@gmail.com>
2023-08-29 18:08:08 +0200

    Thomas Was Late SFML

---
+++ ThomasWasLateSFML/Draw.cpp
@@ -0,0 +1,39 @@
+#include "Engine.h"
+
+void Engine::draw() {
+	m_Window.clear(Color::White);
+
+	if (!m_SplitScreen) {
+		m_Window.setView(m_BGMainView);
+		m_Window.draw(m_BackgroundSprite);
+		m_Window.setView(m_MainView);
+
+        m_Window.draw(m_VALevel, &m_TextureTiles);
+
+        m_Window.draw(m_Thomas.getSprite());
+        m_Window.draw(m_Bob.getSprite());
+	}
+	else { // Split-screen view
+		m_Window.setView(m_BGLeftView);
+		m_Window.draw(m_BackgroundSprite);
+		m_Window.setView(m_LeftView);
+        
+        m_Window.draw(m_VALevel, &m_TextureTiles);
+
+        m_Window.draw(m_Bob.getSprite());
+        m_Window.draw(m_Thomas.getSprite());
+		
+		m_Window.setView(m_BGRightView);
+		m_Window.draw(m_BackgroundSprite);
+		m_Window.setView(m_RightView);
+
+        m_Window.draw(m_VALevel, &m_TextureTiles);
+
+        m_Window.draw(m_Thomas.getSprite());
+        m_Window.draw(m_Bob.getSprite());
+	}
+
+	m_Window.setView(m_HudView);
+	
+	m_Window.display();
+}
---
+++ ThomasWasLateSFML/Update.cpp
@@ -0,0 +1,51 @@
+#include "Engine.h"
+#include <SFML/Graphics.hpp>
+#include <sstream>
+
+using namespace sf;
+
+void Engine::update(float dtAsSeconds) {
+    if (m_NewLevelRequired) {
+        loadLevel();
+    }
+
+	if (m_Playing) {
+        m_Thomas.update(dtAsSeconds);
+        m_Bob.update(dtAsSeconds);
+
+
+        if (detectCollisions(m_Thomas) && detectCollisions(m_Bob)) {
+            m_NewLevelRequired = true;
+        }
+        else {
+            detectCollisions(m_Bob);
+        }
+
+        if (m_Bob.getFeet().intersects(m_Thomas.getHead())) {
+            m_Bob.stopFalling(m_Thomas.getHead().top);
+        }
+        else if (m_Thomas.getFeet().intersects(m_Bob.getHead())) {
+            m_Thomas.stopFalling(m_Bob.getHead().top);
+        }
+
+
+		m_TimeRemaining -= dtAsSeconds;
+
+		if (m_TimeRemaining <= 0) {
+			m_NewLevelRequired = true;
+		}
+	}
+
+    if (m_SplitScreen) {
+        m_LeftView.setCenter(m_Thomas.getCenter());
+        m_RightView.setCenter(m_Bob.getCenter());
+    }
+    else {
+        if (m_Character1) {
+            m_MainView.setCenter(m_Thomas.getCenter());
+        }
+        else {
+            m_MainView.setCenter(m_Bob.getCenter());
+        }
+    }
+}
